2b500d4686f46d63c05756a5a2eea01df2c0372f
kethy26 <135863660+kethy26@users.noreply.github.com>
2023-06-21 17:19:30 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula08ListasRepeticao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> nomes = new List<string>();
+
+            nomes.Add("kethy");
+            nomes.Add("Caroline");
+            nomes.Add("Sofia");
+
+            Console.WriteLine("A primeira posição é " + nomes[0]);
+            Console.WriteLine("A segunda posição é " + nomes[1]);
+            Console.WriteLine("A terceira posição é " + nomes[2]);
+
+            nomes.Remove("Caroline");
+
+
+
+
+
+            Console.ReadKey();
+
+
+        }
+        void Arrays()
+        {
+
+            string[] nomes = new string[3];
+            int[] idade = { 19, 20, 18, 19, 31, 25 };
+
+
+            nomes[0] = "conrado";
+            nomes[1] = "kethy";
+            nomes[2] = "caroline";
+
+
+            Console.WriteLine(" O nome da primeira posição é" + nomes[0]);
+            Console.WriteLine("O nome da segunda posição é " + nomes[0]);
+            Console.WriteLine("O nome da terceira posição é " + nomes[0]);
+
+
+
+
+
+
+            Console.ReadKey();
+
+
+
+
+
+
+            void atividade()
+            {
+                string reposta1;
+                int número;
+                
+                Console.WriteLine("Digite uma opção:");
+                resposta1 = (Console.ReadLine());
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                void Listas()
+                {
+
+                    // Repetições (loop) 
+
+
+                    int contador = 0;
+                   
+                    while(contador < 5 )
+                    {
+
+                        Console.WriteLine("Olá");
+
+
+                    }
+
+
+                    Console.ReadKey();
+
+                }
+            }
+
+            void contéudo novo
+          {
+
+
+
+
+
+           }
+          
+        }
+    }
+    
+
+    
+
+    
+
+
+    
+
+
+
+}
+
